c421fbf905a17c4dd15004dd1b3e740bed9b1bd5
NastiiaV <awerga13@gmail.com>
2021-07-19 12:01:03 +0300

    [ADD 2] added isPlain

--- main.js
+++ main.js
@@ -3,3 +3,6 @@ const { isPlainObject } = require('./objects/isPlain')
 const app = 'App';
 console.log(app);
 
+const data = { a: 1 };
+console.log(isPlainObject(data));
+console.log('Added isPlain func');
